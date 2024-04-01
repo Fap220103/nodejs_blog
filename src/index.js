561855853697e70d38ebf95b58434766cdb8b88d
Phap <prince74427@gmail.com>
2024-04-01 20:19:59 +0700

    update file

--- src/index.js
+++ src/index.js
@@ -1,30 +1,35 @@
 const express = require('express');
 const morgan = require('morgan');
-const hbs  = require('express-handlebars');
-const path = require('path'); 
+const hbs = require('express-handlebars');
+const path = require('path');
 const app = express();
 const port = 3000;
-app.use(express.static(path.join(__dirname,'public')));
 
+const route = require('./routes');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
 //http logger
-app.use(morgan('combined'));
+//app.use(morgan('combined'));
 
 //template engine
-app.engine('hbs', hbs.engine({
-    extname: '.hbs'
-}));
+app.engine(
+    'hbs',
+    hbs.engine({
+        extname: '.hbs', //config name handlerbars = hbs
+    }),
+);
 app.set('view engine', 'hbs');
-app.set('views',path.join(__dirname,'resources/views'));
+app.set('views', path.join(__dirname, 'resources/views'));
 
-
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+// Routes init
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
